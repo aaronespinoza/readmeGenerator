59c6355b782b656c29640c620d2145a3e6df1680
aaronespinoza <aarone@berkeley.edu>
2022-01-17 15:13:26 -0800

    index questions made

--- index.js
+++ index.js
@@ -1,17 +1,67 @@
 // TODO: Include packages needed for this application
 
-const inquirer = require('inquirer');
-const fs = require('fs');
-const generateMarkdown = require('./utils/generateMarkdown')
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: "input",
+    message: "What is your projects title?",
+    name: "title",
+},
+{
+    type: "input",
+    message: "Describe your project?",
+    name: "description",
+},
+{
+    type: "input",
+    message: "How do you use the program?",
+    name: "usage",
+},
+{
+    type: "input",
+    message: "How do you install?",
+    name: "installation",
+},
+{
+    type: "list",
+    message: "What is the license youre using?",
+    name: "license",
+    choices: ["MIT", "GPL_3.0", "none"]
+},
+{
+    type: "input",
+    message: "What tests did you run on your application?",
+    name: "tests",
+
+},
+{
+    type: "input",
+    message: "Please specify contribution guidelines.",
+    name: "contribution",
+
+},
+{
+    type: "input",
+    message: "What is your email?",
+    name: "email",
+
+},
+{
+    type: "input",
+    message: "Please enter your GitHub link",
+    name: "githubUsername",
+
+},];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, (err) =>
-    err ? console.log(err) : console.log('Success!')
+    err ? console.log(err) : console.log("Success!")
 );}
+
 
 // TODO: Create a function to initialize app
 function init() {
