00a1a66b035d5ce9c19a68e5e516fb60eadcc99e
aaronespinoza <aarone@berkeley.edu>
2022-01-17 14:58:56 -0800

    index in progress

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown')
+
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+    err ? console.log(err) : console.log('Success!')
+);}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .prompt(questions)
+    .then((response) => {
+            console.log(response)
+            let README = generateMarkdown(response)
+            writeToFile("README.md", README)
+        }
+);}
 
 // Function call to initialize app
 init();
